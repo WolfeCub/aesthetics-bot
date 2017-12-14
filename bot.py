cfeb060278e07923623d23a0bf58751b261df12b
Josh Wolfe <joshuafwolfe@gmail.com>
2017-12-13 21:42:02 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,61 @@
+import os
+import re
+import json
+import discord
+import asyncio
+import spice_api as spice
+import html2text as h2t
+
+client = discord.Client()
+creds = ()
+config = {}
+
+def setup():
+    global config
+
+    if not os.path.exists('config.json'):
+        print('No config.json file found')
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+
+    if not config['token']:
+        print('No token value found in config file')
+
+
+def create_message_from_results(results):
+    anime = results[0]
+    embed = discord.Embed(
+        title       = anime.title,
+        type        = 'rich',
+        description = re.sub(r'\[.*?\]|\n', '', h2t.html2text(anime.synopsis)),
+        url         = 'https://myanimelist.net/anime/%s' % anime.id,
+        )
+    embed.set_thumbnail(url=anime.image_url)
+    embed.add_field(name='Score', value=anime.score)
+    embed.add_field(name='Episodes', value=anime.episodes)
+    embed.add_field(name='Status', value=anime.status)
+    embed.add_field(name='Aired', value='%s -> %s' % (anime.dates[0], anime.dates[1]))
+    return embed
+
+@client.event
+async def on_ready():
+    global creds
+    creds = spice.load_auth_from_file('auth')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.channel.name == 'anime-recommendation-thread':
+        results = spice.search(message.content, spice.get_medium('anime'), creds)
+        await client.send_message(message.channel,
+                                  embed=create_message_from_results(results))
+
+
+setup()
+client.run(config['token'])
+
