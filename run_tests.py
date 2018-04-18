7691e5319b20447a8b9f7825a09248ca336b5b59
Josh Wolfe <joshuafwolfe@gmail.com>
2018-04-17 22:41:52 -0400

    Removed un-needed code

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,6 @@ import os
 import glob
 from importlib import import_module
 
-modules = []
-
 for item in glob.glob('./mods-available/*_mod.py'):
     name = os.path.basename(item)[:-3]
-    modules.append(import_module(f'mods-available.{name}'))
+    import_module(f'mods-available.{name}')
